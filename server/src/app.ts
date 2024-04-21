a8f7a3c090ce9f94fdd34827dd83551d53ac274c
Saatvik Nargund <saatvik.nargund@gmail.com>
2024-04-21 18:53:48 +0530

    refactored app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,5 @@
 import Express, { NextFunction, Response, Request } from "express"
-import Mongoose from "mongoose"
+import Mongoose, { MongooseError } from "mongoose"
 import Cors from "cors"
 import cookieParser from "cookie-parser"
 import httpErrors from "http-errors"
@@ -9,55 +9,55 @@ import loginUser from "./Routes/Login.js"
 import registerUser from "./Routes/Register.js"
 import userRoute from "./Routes/User.js"
 import quizRoute from "./Routes/Quiz.js"
+import { DB_URL, PORT } from "./Constants/Constants.js"
 
-const PORT = process.env.PORT || 8000
+async function main() {
+    try {
+        await Mongoose.connect(DB_URL);
+        console.log("Successfully connected to MongoDB");
 
-Mongoose.connect(process.env.DB_URL!, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(_ => {
-    console.log("Successfully connected to DBMS");
-})
-    .catch(_ => {
-        console.log("Error in connecting to DBMS");
-    })
+        const app = Express()
+        app.disable('etag')
+        app.use(cookieParser())
+        app.use(Cors({ origin: true, credentials: true }))
+        app.use(Express.json())
 
+        app.use(Express.static(Path.join(import.meta.dirname, "..", "build")))
 
-const app = Express()
-app.disable('etag')
-app.use(cookieParser())
-app.use(Cors({ origin: true, credentials: true }))
-app.use(Express.json())
+        app.use(loginUser)
+        app.use(registerUser)
+        app.use(quizRoute)
+        app.use(userRoute)
 
-app.use(Express.static(Path.join(import.meta.dirname, "..", "build")))
+        app.use((err: httpErrors.HttpError, req: Request, res: Response, next: NextFunction) => {
+            console.log("There is an error");
+            res.status(err.status || 500)
+            return res.json({
+                error: {
+                    status: err.status || 500,
+                    message: err.message || "There is an error"
+                }
+            })
 
-app.use(loginUser)
+        })
 
-app.use(registerUser)
+        app.listen(PORT, () => {
+            console.log("Started listening");
+        })
 
-app.use(quizRoute)
-
-app.use(userRoute)
-
-app.use((err: httpErrors.HttpError, req: Request, res: Response, next: NextFunction) => {
-    console.log("There is an error");
-    res.status(err.status || 500)
-    return res.json({
-        error: {
-            status: err.status || 500,
-            message: err.message || "There is an error"
+    } catch (error) {
+        if (error instanceof MongooseError) {
+            console.log('Failed to connect to mongoDB, exiting');
+        } else {
+            console.log("Some other error");
         }
-    })
-
-})
-
-app.listen(PORT, () => {
-    console.log("Started listening");
-
-})
+    }
+}
 
 process.on('SIGINT', async () => {
     await Mongoose.connection.close()
     console.log("Connection closed");
     process.exit(0)
 })
+
+await main();
