c13de847383fd9fef3228e44173a6cefc0ae8ea6
Saatvik Nargund <saatvik.nargund@gmail.com>
2024-06-08 11:22:07 +0530

    removed typechecking

--- server/src/Constants/Constants.ts
+++ server/src/Constants/Constants.ts
@@ -1,3 +1,3 @@
 export const DB_URL = process.env.DB_URL || "mongodb://localhost:27017";
-export const PORT = process.env.PORT || 8000
+export const PORT = process.env.PORT || 8000;
 export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || 'abcd';
